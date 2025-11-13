da971e03f81a399861508c9d33e6b8aa3f3954a2
Giovanna Vasques <giovanna.vasques1101@hotmail.com>
2025-11-13 18:42:53 -0300

    projeto inicial

---
+++ src/main/java/com/fiap/gs_5_ethicalrecruiting/Gs5EthicalrecruitingApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.gs_5_ethicalrecruiting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gs5EthicalrecruitingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gs5EthicalrecruitingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/gs_5_ethicalrecruiting/Gs5EthicalrecruitingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.gs_5_ethicalrecruiting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gs5EthicalrecruitingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
